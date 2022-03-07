d7d7f1099a705878040ce6bee5ef0aae9d6f020e
AshishKumar409 <ashishkumarnath9@gmail.com>
2022-03-07 14:11:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jmsa2x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jmsa2x)
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'Bleach',
+    content:
+      '15 year old boy who can see ghosts, becomes a soul reaper so he can fight hollows',
+  },
+  {
+    title: 'JoJo Bizzare Adventures',
+    content: 'A bizzare adventure for Joestar bloodline ',
+  },
+  {
+    title: 'one punch man',
+    content: 'Solos your favourite verse',
+  },
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <br />
+      <Accordion items={items} />
+    </div>
+  );
+}
