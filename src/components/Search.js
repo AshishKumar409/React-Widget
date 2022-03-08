27d9baab969fde3ba47e14c9a3319438270c89e9
AshishKumar409 <ashishkumarnath9@gmail.com>
2022-03-08 16:46:29 +0530

    1 file modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,7 +19,6 @@ const Search = () => {
         list: 'search',
         format: 'json',
         origin: '*',
-        proxy: true,
         srsearch: term,
       });
       console.log(response);
